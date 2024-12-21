585c7c8b9d95866676807ee61caf1d768eb95d9b
figo-ut <figosdarley@gmail.com>
2024-12-21 08:11:32 +0200

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ for (let i = 0; i < ownerlist.length; i++) {
 //
 global.botname = process.env.BOTNAME || 'ULTRA-MD';
 global.pairingNumber = process.env.BOT_NUMBER || 'your number';  // put your number here
-global.SESSION_ID = process.env.SESSION_ID || 'session id';  // put your session id here
+global.SESSION_ID = process.env.SESSION_ID || 'jdQlDK4C#lcpFzUGAd2LmEoTUT1UsA3wI_TORF36Yxexg3WsNB8s';  // put your session id here
 
 global.mods = []
 global.prems = []
